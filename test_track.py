ff9c19d6f4f74401b093d3c2917207bec81c6b90
Alexander <trash@openidev.ru>
2016-03-22 15:44:12 +0200

    Add new tests for validation of international number

--- test_track.py
+++ test_track.py
@@ -5,7 +5,7 @@ import unittest
 
 from track import TrackNumber
 
-class TrackNumberTest(unittest.TestCase):
+class TrackNumberParseTest(unittest.TestCase):
 
   def setUp(self):
     self.tn = TrackNumber()
@@ -24,15 +24,45 @@ class TrackNumberTest(unittest.TestCase):
 
   def testSingleTrackParse(self):
     answer = self.tn.parse('RJ255255138CN')
-    self.assertEqual(answer[0]['number'], '255255138')
-    self.assertEqual(answer[0]['first'], 'RJ')
-    self.assertEqual(answer[0]['last'], 'CN')
+    self.assertEqual(answer[0].number, '255255138')
+    self.assertEqual(answer[0].first, 'RJ')
+    self.assertEqual(answer[0].last, 'CN')
 
   def testSingleOnlyNumberTrackParse(self):
     answer = self.tn.parse('11945295679')
-    self.assertEqual(answer[0]['number'], '11945295679')
-    self.assertFalse(answer[0]['first'])
-    self.assertFalse(answer[0]['last'])
+    self.assertEqual(answer[0].number, '11945295679')
+    self.assertFalse(answer[0].first)
+    self.assertFalse(answer[0].last)
+
+class TrackNumberValidTest(unittest.TestCase):
+
+  def setUp(self):
+    self.tn = TrackNumber()
+
+  def testValidOfInternationalTrackNumberFalse(self):
+    answer = self.tn.parse('RJ222222221CN')
+    valid = self.tn.valid(answer[0])
+    self.assertFalse(valid)
+
+  def testValidOfInternationalTrackNumberFalse3(self):
+    answer = self.tn.parse('RK0267053CN')
+    valid = self.tn.valid(answer[0])
+    self.assertFalse(valid)
+
+  def testValidOfInternationalTrackNumberTrue1(self):
+    answer = self.tn.parse('RJ482508186CN')
+    valid = self.tn.valid(answer[0])
+    self.assertTrue(valid)
+
+  def testValidOfInternationalTrackNumberTrue(self):
+    answer = self.tn.parse('RK026705563CN')
+    valid = self.tn.valid(answer[0])
+    self.assertTrue(valid)
+
+  def testValidOfInternationalTrackNumberFalse2(self):
+    answer = self.tn.parse('1194529567922')
+    valid = self.tn.valid(answer[0])
+    self.assertFalse(valid)
 
 if __name__ == '__main__':
   unittest.main()
